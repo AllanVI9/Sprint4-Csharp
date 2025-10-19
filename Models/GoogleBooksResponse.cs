229af39550b24b6dba21a2a423a66b06c50d2d07
Allan <61694786+AllanVI9@users.noreply.github.com>
2025-10-19 15:56:07 -0300

    some improvements

---
+++ Models/GoogleBooksResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace BibliotecaApi.Models
+{
+    public class GoogleBooksResponse
+    {
+        public List<GoogleBookItem> items { get; set; }
+    }
+
+    public class GoogleBookItem
+    {
+        public VolumeInfo volumeInfo { get; set; }
+    }
+
+    public class VolumeInfo
+    {
+        public string? title { get; set; }
+        public List<string>? authors { get; set; }
+    }
+}
